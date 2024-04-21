817b5e5b175a316cfb4b0780ca20cc31d4a8eedc
“MasterGodzilla” <ryansunhanchi@gmail.com>
2024-04-20 22:19:27 -0400

    mistral-7b-instruct-v0.2

---
+++ test_scripts.sh
@@ -0,0 +1,6 @@
+python test.py --model google/gemma-7b-it
+python test.py --model mistralai/Mixtral-8x7B-Instruct-v0.1
+python test.py --model mistralai/Mixtral-8x22B-Instruct-v0.1
+python test.py --model Qwen/Qwen1.5-32B-Chat
+python test.py --model meta-llama/Llama-2-70b-chat-hf
+python test.py --model databricks/dbrx-instruct
